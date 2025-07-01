24e35a30a9de0b56610de58770851155612a3d78
Tafka-4 <shkh071216@gmail.com>
2025-07-01 13:17:17 +0900

    Chore: dev deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 echo "[INFO] Pulling new images"
-docker compose pull --quiet
+sudo docker compose pull --quiet
 
 echo "[INFO] Recreating containers"
-docker compose up -d --remove-orphans
+sudo docker compose up -d --remove-orphans
 
 echo "[INFO] Removing dangling images"
-docker image prune -f
+sudo docker image prune -f
