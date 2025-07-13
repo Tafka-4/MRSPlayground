3f36f3f977f72a4b6dd4f753bfa28981a067e901
Tafka-4 <shkh071216@gmail.com>
2025-07-13 16:32:46 +0900

    Fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 set -euo pipefail
 
 echo "[INFO] Pulling new images"
-sudo docker compose pull --quiet
+docker compose pull --quiet
 
 echo "[INFO] Stopping old containers"
-sudo docker compose down --remove-orphans
+docker compose down --remove-orphans
 
 echo "[INFO] Removing old images"
-sudo docker image prune -f
+docker image prune -f
 
 echo "[INFO] Starting new containers"
-sudo docker compose up --build -d --remove-orphans
+docker compose up --build -d --remove-orphans
 
-sudo rm /opt/app/MRSPlayground/.env
+rm /opt/app/MRSPlayground/.env
