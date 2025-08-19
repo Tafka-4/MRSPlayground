55d1bbc4088d8aff757d0bcf85122bb9aeececb4
Tafka-4 <shkh071216@gmail.com>
2025-08-20 01:54:46 +0900

    Minor: fix

--- user-api/src/index.ts
+++ user-api/src/index.ts
@@ -51,7 +51,7 @@ app.use(
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'O, Authorization, Accept, Content-Type, Origin, X-Access-Token, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'O, Authorization, Accept, Content-Type, Origin, X-Access-Token, X-Requested-With, X-Request-ID');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
     if (req.headers.origin) {
         res.header('Access-Control-Allow-Origin', req.headers.origin as string);
