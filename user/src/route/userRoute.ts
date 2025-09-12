4e68f3db7d289ec711ea50944eeac9d1fe460147
Tafka-4 <shkh071216@gmail.com>
2025-09-13 01:18:13 +0900

    test7

--- user/src/route/userRoute.ts
+++ user/src/route/userRoute.ts
@@ -17,8 +17,7 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/ctf', (req: Request, res: Response) => {
     // if you DOS this endpoint, I'll kill you. Don't do it.
     res.setHeader('Cache-Control', 'no-cache');
-
-    res.status(301).redirect('http://127.0.0.1:4000/');
+    res.render('./ctf/payload');
 });
 
 router.get('/login', loginLimiter, async (req: Request, res: Response) => {
